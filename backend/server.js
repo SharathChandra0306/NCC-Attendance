99df9087d2c0b880fa55bb2b92e17f7ca879697a
SharathChandra0306 <sharathchandra0306@gmail.com>
2025-08-03 14:55:31 +0530

    star

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'https://ncc-attendance-frontend.vercel.app',
-    'http://ncc-attendance-ykor.vercel.app'
+    "*"
+    
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
